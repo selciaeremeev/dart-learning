0a9c0e5d104122d316820b2f03dc935f6a53ab50
Selcia <selcia.eremeev@gmail.com>
2021-04-29 03:36:40 +0900

    add

---
+++ values.dart
@@ -0,0 +1,17 @@
+
+void main() {
+  num ex_num = 1;
+  int ex_int = 10;
+  double ex_double = 10.0;
+  bool ex_bool = true;
+  String ex_string = "test";
+  List ex_list = ["t", "e", "s", "t"];
+  Map ex_map = {"hello": "world"};
+  print("num: " + ex_num.toString());
+  print("int: " + ex_int.toString());
+  print("double: " + ex_double.toString());
+  print("bool: " + ex_bool.toString());
+  print("String: " + ex_string);
+  print("List: " + ex_list.toString());
+  print("Map: " + ex_map["hello"]);
+}
