1bd22c6cd03a9c1ea41512f64bb02a9d77eda16f
Selcia <selcia.eremeev@gmail.com>
2021-04-30 22:05:48 +0900

    add conditions

---
+++ conditions.dart
@@ -0,0 +1,8 @@
+void main() {
+  int n = 10;
+  if (n % 2 == 0) {
+    print("${n}は偶数です。");
+  } else {
+    print("${n}は奇数です。");
+  }
+}
--- functions.dart
+++ functions.dart
@@ -6,7 +6,12 @@ void function2(String string1, String string2) {
   print(string1 + string2);
 }
 
+String function3(String string1, String string2) {
+  return string1 + string2;
+}
+
 void main() {
   function(string1: "hello", string2: "world");
   function2("hello", "world");
+  print(function3("hello", "world"));
 }
